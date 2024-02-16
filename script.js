42c2d981a08bb702578ffd7a17a7f17ca8c9ccee
sumet02468 <56708580+sumet02468@users.noreply.github.com>
2024-02-16 10:36:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function myFunction() {
+$(function(){  // link menu
+    $("#nav-placeholder").load("nav.html");
+    });
+
+function myFunction() {   //menu responsive
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
